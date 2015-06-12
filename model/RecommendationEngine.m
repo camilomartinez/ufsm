975d44ef650ca4aeb850a078c8d1409155cf23f1
camilomartinez <marscamand@gmail.com>
2015-06-12 21:47:44 +0200

    Generate recommendation file for a single fold

--- model/RecommendationEngine.m
+++ model/RecommendationEngine.m
@@ -1,4 +1,4 @@
-classdef RecommendationEngine
+classdef RecommendationEngine < handle
     %RECOMMENDATIONENGINE Generates recommendations for given inputs
     %   Manages recommendation workflow
     
@@ -7,16 +7,39 @@ classdef RecommendationEngine
         RecommenderBuilder
     end
     
+    properties (Access = private, Constant)
+        FileFormat = '%s\\%s_%i.csv';
+        RecFileName = 'recs';
+        TrainFileName = 'train';
+    end
+    
     methods
         %Constructor
         function obj = RecommendationEngine(builder)
             obj.RecommenderBuilder = builder;
         end
         
-        function recommendFolds(nFolds, trainFolder, recommendationFolder)
-            
+        function recommendFolds(obj, nFolds, trainFolder, recommendationFolder)
+            trainFilePath = obj.trainFilePathForFold(trainFolder, 1);
+            dataModel = DataModel(trainFilePath);
+            recommender = obj.RecommenderBuilder(dataModel);
+            recommender.train();
+            recommender.recommend();
+            recFilePath = obj.recFilePathForFold(recommendationFolder, 1);
+            dlmwrite(recFilePath, recommender.Recommendations, '\t');
         end
     end
     
+    methods(Access = private)
+        function path = trainFilePathForFold(obj, trainFolder, i)
+            path = sprintf(obj.FileFormat,...
+                trainFolder, obj.TrainFileName, i-1);
+        end
+        
+        function path = recFilePathForFold(obj, recFolder, i)
+            path = sprintf(obj.FileFormat,...
+                recFolder, obj.RecFileName, i-1);
+        end
+    end
 end
 
--- test/model/RecommendationEngineTest.m
+++ test/model/RecommendationEngineTest.m
@@ -13,10 +13,35 @@ function builderTest(testCase)
     testCase.verifyInstanceOf(recommender, ?Recommender);
 end
 
+function recommendFoldsTest(testCase)
+    engine = testCase.TestData.engine;
+    recFolder = testCase.TestData.recommendationFolder;
+    engine.recommendFolds(1,...
+        testCase.TestData.trainFolder, recFolder);
+    recFilePath = strcat(recFolder,'\recs_0.csv');
+    % Use java as interface is simpler
+    % returns bool coded as 1 if true
+    recFileExists = java.io.File(recFilePath).exists() == 1;
+    testCase.verifyTrue(recFileExists,...
+        'The recommendation file is not generated');
+end
+
+%% Optional file fixtures  
+function setupOnce(testCase)  % do not change function name
+    sampleDataFolder = 'C:\Code\Polimi\thesis\Matlab\data\rivalsample\';
+	testCase.TestData.trainFolder = strcat(sampleDataFolder, 'model');
+    testCase.TestData.recommendationFolder =...
+        strcat(sampleDataFolder, 'recommendations');
+end
+
 %% Optional fresh fixtures  
 function setup(testCase)  % do not change function name
 	builder = @PopularRecommender;
     engine = RecommendationEngine(builder);    
     testCase.TestData.engine = engine;
+    % Clear recommendation files if any
+    filesToDelete = strcat(testCase.TestData.recommendationFolder,...
+        '\recs_*.csv');
+    delete(filesToDelete);
 end
 
