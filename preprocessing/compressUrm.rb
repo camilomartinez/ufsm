3ca758eb9a9b48879701b228b746efb2fd5a49b5
camilomartinez <marscamand@gmail.com>
2015-06-18 13:14:25 +0200

    Properly generate urm using item cache to match ICM

--- preprocessing/compressUrm.rb
+++ preprocessing/compressUrm.rb
@@ -1,5 +1,7 @@
 require 'set'
 
+start = Time.now
+puts 'Read item cache'
 #Read itemCache
 itemCacheFile = 'itemcache_icm.dat'
 separator = '|'
@@ -16,11 +18,15 @@ File.foreach(itemCacheFile) do |line|
 end
 # Size check
 raise 'Wrong item cache size' unless itemCache.size == 6489
+puts "Elapsed so far #{Time.now - start} seconds"
 
+puts 'Read urm'
 # Read all user Ids in URM
 urmFile = 'urm.dat'
 isHeader = true
 userIdSet = SortedSet.new
+Preference = Struct.new(:userId, :itemId, :rating)
+preferences = []
 File.foreach(urmFile) do |line|
   # Skip first line as it is the header
   if isHeader
@@ -29,6 +35,9 @@ File.foreach(urmFile) do |line|
   end
   parts = line.split(separator)
   userId = parts[0].to_i
+  itemId = parts[1]
+  rating = parts[2].chomp
+  preferences << Preference.new(userId, itemId, rating)
   userIdSet.add userId
 end
 raise 'Wrong user id set size' unless userIdSet.count == 247939
@@ -39,25 +48,20 @@ userIdSet.each do |oldId|
   userCache[oldId] = newId
   newId += 1
 end
+puts "Elapsed so far #{Time.now - start} seconds"
 
+puts 'Write urm'
 # Replace user and item ids using cache
 urmNewFile = 'urmCache.dat'
 File.open(urmNewFile, 'w') do |file|
-  File.foreach(urmFile) do |line|
-    # First line is the header
-    if isHeader
-      lineToWrite = "USERID|ITEMID|RATING"
-      file.write(lineToWrite)
-      isHeader = false
-      next
-    end
-    parts = line.split(separator)
-    userId = parts[0].to_i
-    itemId = parts[1]
-    rating = parts[2]
-    newUserId = userCache[userId]
-    newItemId = itemCache[itemId]
-    lineToWrite = "#{newUserId}|#{newItemId}|#{rating}"
+  #Header
+  lineToWrite = "USERID|ITEMID|RATING\n"
+  file.write(lineToWrite)
+  preferences.each do |preference|
+    newUserId = userCache[preference.userId]
+    newItemId = itemCache[preference.itemId]
+    lineToWrite = "#{newUserId}|#{newItemId}|#{preference.rating}\n"
     file.write(lineToWrite)
   end
 end
+puts "Finished in #{Time.now - start} seconds"
