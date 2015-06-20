e6128064181efb4f16c6811b3f8a36ff56021662
camilomartinez <marscamand@gmail.com>
2015-06-20 12:15:23 +0200

    Enhance benchmark information and use short netflix data

--- test/performance/RecommenderBenchmark.m
+++ test/performance/RecommenderBenchmark.m
@@ -2,14 +2,25 @@
 clear;
 clc;
 %% Test files
-sampleDataFolder = 'C:\Code\Polimi\thesis\Matlab\data\rivalsample\';
+sampleDataFolder = 'C:\Code\Polimi\thesis\Matlab\evaluation\data\netflix\';
 trainFolder = strcat(sampleDataFolder, 'model');
 recFolder = strcat(sampleDataFolder, 'recommendations');
 filesToDelete = strcat(recFolder,'\recs_*.csv');
 delete(filesToDelete);
 %% Recommendation benchmark
-engine = RecommendationEngine(@PopularRecommender);
-engine.recommendFolds(1, trainFolder, recFolder);
+engine = RecommendationEngine(@CoSimRecommender);
+engine.recommendFolds(5, trainFolder, recFolder);
+disp('Training time per fold:')
 disp(engine.TrainingTimePerFold)
+disp('Recommendation time per fold:')
 disp(engine.RecommendationTimePerFold)
+disp('Writing time per fold:')
 disp(engine.WritingTimePerFold)
+disp('Baseline CoSimRecommender')
+meanStats = sum([...
+    engine.TrainingTimePerFold',...
+    engine.RecommendationTimePerFold',...
+    engine.WritingTimePerFold']);
+fprintf(...
+    'Avg. training time %g\nAvg. recommendation time %g\nAvg. writing time %g\n',...
+    meanStats(1),meanStats(2),meanStats(3))
